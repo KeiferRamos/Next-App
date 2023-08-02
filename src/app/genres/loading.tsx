d89deb97148c9095627e461ea193d787d391a26e
keifer ramos <keiferramos@keifers-MacBook-Air.local>
2023-08-02 15:53:43 +0800

    fefefe

---
+++ src/app/genres/loading.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Loader } from "../styles";
+
+function Loading() {
+  return (
+    <Loader>
+      <div>
+        <img src="/images/wait.png" alt="" />
+        <p>loading...</p>
+      </div>
+    </Loader>
+  );
+}
+
+export default Loading;
